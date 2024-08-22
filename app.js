a973dbe46fc15f9c67b83c97319cf76bf8ed7413
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-22 10:46:18 +0000

    first

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+export default app;
+
+
+
+/*
+app.get('/', (req,res) => {
+    res.send("Hello Devs")
+});
+
+app.listen(3000, ()=> {
+    console.log('Express server intialized ');
+});
+*/
