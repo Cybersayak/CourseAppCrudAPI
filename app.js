fe41962db9d5989b7b56496c38832bafb78c26f4
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-08 07:51:49 +0000

    Mongo Connected

--- app.js
+++ app.js
@@ -5,13 +5,3 @@ const app = express();
 export default app;
 
 
-
-/*
-app.get('/', (req,res) => {
-    res.send("Hello Devs")
-});
-
-app.listen(3000, ()=> {
-    console.log('Express server intialized ');
-});
-*/
--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import mongoose from "mongoose";
-import app from "./app.js";
+import app from "./app.js"
+
+( async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/demo")
+        console.log("DB connected ");
+
+        const onListening = () => {
+            console.log("Listening to port 5000");
+        }
+
+        app.listen(5000, onListening)
+    } catch (error) {
+        console.log("error :" , error);
+        throw error ;
+    }
+}) ()
